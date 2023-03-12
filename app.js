239d11690f73e2399da10215226c391ab379671b
saloni2811 <saloni28singh2001@gmail.com>
2023-03-13 00:30:44 +0530

    Changes

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/" , function(req,res){
+  res.sendFile(__dirname + "/sign-up.html");
+});
+
+app.post("/", function(req,res){
+    var firstName =req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+    console.log(firstName , lastName , email);
+
+})
 
 
 
 
 
 
-app.listen(3000 , function(){
-    console.log("server is running at port 3000");
+app.listen(5500 , function(){
+    console.log("server is running at port 5500");
 });
